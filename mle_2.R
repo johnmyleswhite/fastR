b7b34d5008eb6fa234ace914f6b14be8008633ef
John Myles White <jmw@johnmyleswhite.com>
2011-06-19 01:06:00 -0400

    Added explicit return()

--- mle_2.R
+++ mle_2.R
@@ -3,7 +3,7 @@ rowwise.likelihood <- function(row, a, delta)
   u2 <- discounted.value(row['X2'], row['T2'], delta)
   u1 <- discounted.value(row['X1'], row['T1'], delta)
   p <- invlogit(a * (u2 - u1))
-  ifelse(row['C'] == 1, p, 1 - p)
+  return(ifelse(row['C'] == 1, p, 1 - p))
 }
 
 log.likelihood <- function(choices, a, delta)
